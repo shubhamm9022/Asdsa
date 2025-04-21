92940eb4d3678958446229f8a6e234ba147d4ee5
shubhamm9022 <shubhamwaghmode006@gmail.com>
2025-04-21 22:26:02 +0530

    Add files via upload

---
+++ Request.tsx
@@ -0,0 +1,8 @@
+export default function Request() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-2">Request a Movie</h1>
+      <p>You can request a movie on our <a className="text-blue-600 underline" href="https://t.me/movievaultHQ">Telegram Channel</a>.</p>
+    </div>
+  )
+}
