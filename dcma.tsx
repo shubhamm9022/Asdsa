6ebec9e4ce4af6a458046ab8bac34c09a4b3aa2a
shubhamm9022 <shubhamwaghmode006@gmail.com>
2025-04-21 22:27:53 +0530

    Delete dcma.tsx

--- dcma.tsx
+++
@@ -1,8 +0,0 @@
-export default function DMCA() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold mb-2">DMCA Removal</h1>
-      <p>If you own rights to any content and want it removed, please contact us via Telegram or email.</p>
-    </div>
-  )
-}
